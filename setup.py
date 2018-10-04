2e3bdda83333963e7a35fd741aed80e28bea4410
Ajay Singh <a.11.singh@nokia.com>
2018-10-04 14:55:33 -0700

    fourth commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'Ajay-app',
     version = '1.0.0',
     description='Step 1 tutorial',
-    url = '',
+    url = 'https://github.com/ajaycloud9/my_sample_app.git',
     author_email='singh.ajay@husky.neu.edu'
     license='MIT'
     packages=find_packages()
