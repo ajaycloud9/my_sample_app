ce4dea617777c828bc1b0f86e84b0198bed450e7
Ajay Singh <a.11.singh@nokia.com>
2018-10-04 14:37:55 -0700

    second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Ajay-app',
+    version = '1.0.0',
+    description='Step 1 tutorial',
+    url = '',
+    author_email='singh.ajay@husky.neu.edu'
+    license='MIT'
+    packages=find_packages()
+
+)
