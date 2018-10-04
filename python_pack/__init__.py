2d18ae72364a6b911707913f3672adb5ba6aa3d7
Ajay Singh <a.11.singh@nokia.com>
2018-10-04 15:42:50 -0700

    fifth commit

---
+++ python_pack/__init__.py
@@ -0,0 +1,2 @@
+from python_pack.package1.pack_one import print_my_name_1
+from python_pack.package2.pack_two import print_my_name_2
--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'Ajay-app',
+    name = 'python_pack',
     version = '1.0.0',
     description='Step 1 tutorial',
     url = 'https://github.com/ajaycloud9/my_sample_app.git',
-    author_email='singh.ajay@husky.neu.edu'
-    license='MIT'
+    author_email='singh.ajay@husky.neu.edu',
     packages=find_packages()
 
 )
